d042a60ec0967b8d920e706a22ea65d6e603b266
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-05-30 12:12:55 +0200

    remove unused pane

--- src/main.js
+++ src/main.js
@@ -18,14 +18,6 @@ const config = {
 }
 const pane = new Pane()
 
-pane
-	.addBinding(config, 'example', {
-		min: 0,
-		max: 10,
-		step: 0.1,
-	})
-	.on('change', (ev) => console.log(ev.value))
-
 /**
  * Scene
  */
